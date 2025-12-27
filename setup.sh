f3fcb53a83c15e82a9f670a39a55bcbcadb5f051
Korbielowski <dawidstasinski12@gmail.com>
2025-12-27 22:16:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,20 @@
-command -v curl || { echo 'curl command not found, you must install it' ; exit 1; }
-command -v uv || { curl -LsSf https://astral.sh/uv/install.sh | sh; }
+command -v psql || { echo 'Psql/PostgreSQL not found, please install it first, in order to run the program.' ; exit 1; }
+command -v weasyprint || { echo 'Weasyprint not found, please install it first, in order to run the program.' ; exit 1; }
 
-uv sync
+if [[ command -v uv ]]; then
+    uv sync --no-dev --frozen --compile-bytecode --no-cache
+    uv run --no-sync --compile-bytecode playwright install --with-deps chromium
+    if [[ "$1" == "--run" ]]; then
+        uv run --no-sync --compile-bytecode fastapi run backend/app.py
+    fi
+else
+    echo 'Uv not found, please install it first, as it is a recommended way of installing and running the program.'
+    echo 'Do you want to continue without uv? [y/N]'
+    read response
+    if [[ "$response" == "n" || "$response" == "no" || "$response" == "No" ]]; then
+        echo 'Installation cancelled.'
+        exit 1
+    fi
+    echo 'Proceeding with installation without uv.'
+    command -v python || {command -v python3 || { echo 'Python not found, please install it first, in order to run the program'; exit 1; } }
+fi
