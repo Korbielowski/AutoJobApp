fbcde31a21b208bdd3bae6a4a7949ab59ffc59ec
Korbielowski <dawidstasinski12@gmail.com>
2025-12-28 01:00:25 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,35 @@
+#!/bin/sh
+set -eu
+
 command -v psql || { echo 'Psql/PostgreSQL not found, please install it first, in order to run the program.' ; exit 1; }
 command -v weasyprint || { echo 'Weasyprint not found, please install it first, in order to run the program.' ; exit 1; }
 
-if [[ command -v uv ]]; then
+sudo -u postgres psql -c "CREATE DATABASE autoapply;"
+
+if command -v uv; then
     uv sync --no-dev --frozen --compile-bytecode --no-cache
     uv run --no-sync --compile-bytecode playwright install --with-deps chromium
-    if [[ "$1" == "--run" ]]; then
+    if [ "$1" == "--run" ]; then
         uv run --no-sync --compile-bytecode fastapi run backend/app.py
     fi
 else
     echo 'Uv not found, please install it first, as it is a recommended way of installing and running the program.'
     echo 'Do you want to continue without uv? [y/N]'
     read response
-    if [[ "$response" == "n" || "$response" == "no" || "$response" == "No" ]]; then
+    if [ "$response" == "n" || "$response" == "no" || "$response" == "No" ]; then
         echo 'Installation cancelled.'
         exit 1
     fi
     echo 'Proceeding with installation without uv.'
-    command -v python || {command -v python3 || { echo 'Python not found, please install it first, in order to run the program'; exit 1; } }
+    if command -v python3; then
+        PYTHON=python3
+    elif command -v python; then
+        PYTHON=python
+    else
+        echo 'Python not found, please install it first, in order to run the program'
+        exit 1
+    fi
+    $PYTHON -m venv .venv && source .venv/bin/activate
+    $PYTHON -m pip install --requirement requirements.txt
+    fastapi run backend/app.py
 fi
