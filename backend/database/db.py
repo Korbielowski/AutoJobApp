f5084e519d846b363b10bf1be2abaf1c7884dc5b
Korbielowski <dawidstasinski12@gmail.com>
2026-01-17 20:10:26 +0100

    Enable foreign keys for sqlite database

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, text
 
 from backend.config import settings
 
@@ -7,3 +7,7 @@ engine = create_engine(settings.DATABASE_URI)
 
 def init_db() -> None:
     SQLModel.metadata.create_all(engine)
+    # Enable foreign keys for local sqlite database
+    if settings.DB_BACKEND == "sqlite":
+        with engine.connect() as connection:
+            connection.execute(text("PRAGMA foreign_keys=ON"))
