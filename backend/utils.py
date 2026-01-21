42eecedc9471265b452df9dd7464f00778455973
Korbielowski <dawidstasinski12@gmail.com>
2026-01-21 22:22:11 +0100

    Move log_agent_run_data function to utils.py file

--- backend/utils.py
+++ backend/utils.py
@@ -1,10 +1,15 @@
 from functools import wraps
 import time
-from typing import Callable
+from typing import Callable, TypeVar
+
+from pydantic_ai import AgentRunResult
+from pydantic import BaseModel
+from devtools import pformat
 
 from backend.logger import get_logger
 
 
+T = TypeVar("T", bound=BaseModel)
 logger = get_logger()
 
 
@@ -20,3 +25,19 @@ def measure_func_time(func: Callable):
         return result
 
     return wrapper
+
+
+def log_agent_run_data(result: AgentRunResult[T]):
+    input_tokens = result.usage().input_tokens
+    output_tokens = result.usage().output_tokens
+    logger.info(
+        f"Agent name: {result.response.model_name}\nTotal cost: {
+            result.response.cost()
+        }\nTotal tokens used: {
+            input_tokens + output_tokens
+        }\nInput tokens used: {input_tokens}\nOutput tokens used: {
+            output_tokens
+        }\nFinal agent output{pformat(result.output)}\nFinish reason: {
+            result.response.finish_reason
+        }"
+    )
