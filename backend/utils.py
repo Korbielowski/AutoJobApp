e5bf8476bc206cc30c699f5ced21cc0ff50e3aca
Korbielowski <dawidstasinski12@gmail.com>
2026-01-23 17:22:41 +0100

    Update log_agent_run_data function

--- backend/utils.py
+++ backend/utils.py
@@ -9,6 +9,20 @@ from devtools import pformat
 from backend.logger import get_logger
 
 
+_LOG_STRING = """
+Agent name: {agent_name}
+Model name: {model_name}
+Agent provider: {provider_name}
+===============================================================
+Input tokens used: {input_tokens}\tInput price: {input_price}$
+---------------------------------------------------------------
+Output tokens used: {output_tokens}\tOutput price: {output_price}$
+---------------------------------------------------------------
+Total tokens used: {total_tokens}\tTotal price: {total_price}$
+===============================================================
+Finish reason: {finish_reason}
+Final agent output: {final_output}
+"""
 T = TypeVar("T", bound=BaseModel)
 logger = get_logger()
 
@@ -27,17 +41,23 @@ def measure_func_time(func: Callable):
     return wrapper
 
 
-def log_agent_run_data(result: AgentRunResult[T]):
+def log_agent_run_data(agent_name: str, result: AgentRunResult[T]):
     input_tokens = result.usage().input_tokens
     output_tokens = result.usage().output_tokens
-    logger.info(
-        f"Agent name: {result.response.model_name}\nTotal cost: {
-            result.response.cost()
-        }\nTotal tokens used: {
-            input_tokens + output_tokens
-        }\nInput tokens used: {input_tokens}\nOutput tokens used: {
-            output_tokens
-        }\nFinal agent output{pformat(result.output)}\nFinish reason: {
-            result.response.finish_reason
-        }"
-    )
+    costs = result.response.cost()
+
+    d = {
+        "agent_name": agent_name,
+        "model_name": result.response.model_name,
+        "provider_name": result.response.provider_name,
+        "input_tokens": input_tokens,
+        "input_price": float(costs.input_price),
+        "output_tokens": output_tokens,
+        "output_price": float(costs.output_price),
+        "total_tokens": input_tokens + output_tokens,
+        "total_price": float(costs.total_price),
+        "final_output": pformat(result.output),
+        "finish_reason": result.response.finish_reason,
+    }
+
+    logger.info(_LOG_STRING.format_map(d))
