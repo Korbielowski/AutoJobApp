7eedca9943b4d9d70df2ea338fd0b9bdf59c74ad
Korbielowski <dawidstasinski12@gmail.com>
2026-01-24 22:04:01 +0100

    Add automation steps to each action

--- backend/scrapers/page_actions.py
+++ backend/scrapers/page_actions.py
@@ -8,7 +8,11 @@ from backend.config import settings
 from backend.database.models import WebsiteModel
 from backend.logger import get_logger
 from backend.schemas.llm_responses import InputFieldTypeEnum, ToolResult
-from backend.scrapers.page_processing import find_html_tag_v2
+from backend.schemas.models import Step
+from backend.scrapers.page_processing import (
+    find_html_tag_v2,
+    read_key_from_mapping_store,
+)
 
 logger = get_logger()
 _action_lock = asyncio.Lock()
@@ -26,7 +30,7 @@ async def goto(page: Page, url: str, retry: int = 3) -> None:
             logger.exception("Timeout for goto")
 
 
-async def click(page: Page, text: str) -> ToolResult:
+async def click(page: Page, text: str, steps: list[Step]) -> ToolResult:
     async with _action_lock:
         tag = await find_html_tag_v2(page=page, text=text)
         if not tag:
@@ -39,6 +43,14 @@ async def click(page: Page, text: str) -> ToolResult:
             await tag.click(force=True)
             await page.wait_for_load_state("load")
             logger.info("click call was successful")
+            steps.append(
+                Step(
+                    function="click",
+                    tag=await read_key_from_mapping_store(text),
+                    additional_arguments={},
+                )
+            )
+
             return ToolResult(success=True)
         except PlaywrightTimeoutError:
             logger.error("click call was not successful, TIMEOUT")
@@ -50,6 +62,7 @@ async def fill(
     text: str,
     input_type: Literal["email", "password"],
     website_info: WebsiteModel,
+    steps: list[Step],
 ) -> ToolResult:
     async with _action_lock:
         tag = await find_html_tag_v2(page=page, text=text)
@@ -73,6 +86,13 @@ async def fill(
                 value, delay=random.randint(2, 12) * 100
             )
             logger.info("fill was successful")
+            steps.append(
+                Step(
+                    function="fill",
+                    tag=await read_key_from_mapping_store(text),
+                    additional_arguments={"input_type": input_type},
+                )
+            )
             return ToolResult(success=True)
         except PlaywrightTimeoutError:
             logger.error("fill was not successful, TIMEOUT")
