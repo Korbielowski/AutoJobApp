4dd3107bcc1e18f22787ad4c3b3120eb3858cfb2
Korbielowski <dawidstasinski12@gmail.com>
2026-02-01 22:10:58 +0100

    Add per model routers

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -1,7 +1,21 @@
 from fastapi import APIRouter
 
-from backend.routes import pages, users
+from backend.routes import pages, account
+from backend.routes.crud import tools, user_preferences, users, projects, charities, languages, programming_languages, locations, educations, job_entries, experiences, social_platforms, websites
 
 api_router = APIRouter()
 api_router.include_router(pages.router)
+api_router.include_router(account.router)
 api_router.include_router(users.router)
+api_router.include_router(user_preferences.router)
+api_router.include_router(tools.router)
+api_router.include_router(projects.router)
+api_router.include_router(charities.router)
+api_router.include_router(languages.router)
+api_router.include_router(programming_languages.router)
+api_router.include_router(locations.router)
+api_router.include_router(educations.router)
+# api_router.include_router(job_entries.router)
+api_router.include_router(experiences.router)
+api_router.include_router(social_platforms.router)
+api_router.include_router(websites.router)
