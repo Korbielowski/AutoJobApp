7780d01ddf540408cf95b7e834e14810aeb2b5a0
Korbielowski <dawidstasinski12@gmail.com>
2026-02-03 23:07:44 +0100

    Add authentication for static files

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,19 @@ from backend.utils import return_exception_response
 
 logger = get_logger()
 
+async def verify_user(q):
+    pass
+
+class AuthStaticFiles(StaticFiles):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    async def __call__(self, scope, receive, send) -> None:
+
+        request = Request(scope, receive)
+        await verify_user(request)
+        await super().__call__(scope, receive, send)
+
 
 @asynccontextmanager
 async def setup(inner_app: FastAPI) -> AsyncGenerator:
@@ -25,7 +38,7 @@ async def setup(inner_app: FastAPI) -> AsyncGenerator:
 
     inner_app.mount(
         path="/",
-        app=StaticFiles(directory=settings.ROOT_DIR / "_static", html=True),
+        app=AuthStaticFiles(directory=settings.ROOT_DIR / "_static", html=True),
         name="static",
     )
 
